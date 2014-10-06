e9344d1a6098144872e6f7bdefc1b6cd2242cd78
surveymedia.ca <trevor@surveymedia.ca>
2014-10-05 21:16:57 -0400

    automated commit

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import *
 import fabric.contrib.project as project
-import os
+import os,re
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = '.'
@@ -29,6 +29,43 @@ def deploy():
     local('git add .')
     local('git commit -am "automated commit"')
     local('git push origin master')
+
+
+
+def create_post(notebook_name):
+    ipython_file_path=os.path.join('/home/trevor/Notebooks',notebook_name +'.ipynb')
+    
+    assert os.path.isfile(ipython_file_path),ipython_file_path
+    metadata_file_path=os.path.abspath(os.path.join(env.content_path,notebook_name+'.meta'))
+    
+    assert os.path.isfile(metadata_file_path)
+    
+    rst_file_path=os.path.abspath(os.path.join(env.content_path,notebook_name+'.rst'))
+    
+    #This is a problem
+    os.chdir(env.content_path)
+    
+    command='ipython nbconvert --to rst {0}'.format(ipython_file_path)
+
+    local(command)
+    
+    meta=file(metadata_file_path).read()
+    
+    
+    rst=file(rst_file_path).read() 
+ 
+    rst=re.compile(r'^\.\.\scode::', re.MULTILINE).sub('.. code-block::',rst)
+    
+    #replace double back-ticks with :code:`...`
+    rst=re.compile(r'``(.*)``',re.MULTILINE).sub(r':code:`\g<1>`',rst)
+    
+    with file(rst_file_path,'w') as out:
+        out.write(meta)
+        out.write(rst)
+        
+    
+    print 'done'
+    
 
 #want a deploy function as well.
 
